b45f7a4ebba52340d13081634aaf7a7d7da222dc
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 03:41:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,14 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  collection = []
+  newtext = ""
+  array.each do |n|
+    newtext = n.capitalize + "!"
+    collection << newtext
+  end
+  collection
 end
 
 def long_planeteer_calls# code an argument here
