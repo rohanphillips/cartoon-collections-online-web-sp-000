03e80b73be4893e722aa978884dc14c32564b8ed
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 04:12:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,7 @@ def find_the_cheese(array)
   # the array below is here to help
   returncheese = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
+  puts array
   array.each_with_index do |n, index|
     if cheese_types.include?(n)
       returncheese = array[index]
