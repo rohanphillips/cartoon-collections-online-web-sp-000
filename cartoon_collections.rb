0d17dc283338ebaf6f3f4da8c3cfcdea56561a9f
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 03:51:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def summon_captain_planet(array)
   collection
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  collection = array.select{|num| num.length > 4 ? true : false}
 end
 
 def find_the_cheese# code an argument here
